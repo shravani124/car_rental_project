9e96d4a70b23a9ffba16906f4f91a64c8e29408f
shravani124 <radeshravani@gmail.com>
2024-04-15 18:57:40 +0530

    done

--- psucess.php
+++ psucess.php
@@ -70,7 +70,7 @@
     <body>
        
       <div class="card">
-      <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+      <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5 ; margin:0 auto;">
         <i class="checkmark">✓</i>
       </div>
         <h1>Success</h1> 
